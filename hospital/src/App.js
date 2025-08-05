0a7affcb9e847a32bc69e441f258aa589b5c6d39
jumanaahmed <jumanaahmed481@gmail.com>
2025-08-05 03:05:00 +0300

    footer

--- hospital/src/App.js
+++ hospital/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import TeamSection from './components/Team/TeamSection';
 
+import logo from './logo.svg';
+import './App.css';
+import Footer from './components/footer/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 function App() {
   return (
     <div>
+
       <TeamSection />
+      <Footer/>
     </div>
   );
 }
---
+++ hospital/src/components/assets/assets.js
@@ -0,0 +1,13 @@
+import footerlogo from './logo.png';
+import facebook from './facebook.png';
+import instagram from './instagram.png';
+import linkedin from './linkedin.png';
+import twitter from './twitter.png';
+
+export {
+  footerlogo,
+  facebook,
+  instagram,
+  linkedin,
+  twitter
+};
